1cb747800e7bdada490b7f107ad14ad6c8769d8d
Jiabo Yu <47837198+SchroDog2@users.noreply.github.com>
2023-04-07 17:41:34 -0400

    add gram-schmidt and mod. gram-schmidt

--- python/qr.py
+++ python/qr.py
@@ -1,10 +1,74 @@
-# qr factorization
+import numpy as np
 
 
-def classical_gram_schmidt():
+def classical_gram_schmidt(A):
     """
-    The classic Gram-Schmidt algorithm is the first thing you might think of for producing an orthogonal set of vectors. For each vector in your data set, remove its projection onto the data set, normalize what is left, and include it in the orthogonal set. Here is the code. X is the original set of vectors, Q is the resulting set of orthogonal vectors, and R is the set of coefficients, organized into an upper triangular matrix.
+    For each vector in the matrix, remove its projection onto the data set, 
+    normalize what is left, and include it in the orthogonal set. 
 
-
+    X is the original set of vectors 
+    Q is the resulting set of orthogonal vectors
+    R is the set of coefficients, organized into an upper triangular matrix.
     """
-    pass
+    if not isinstance(A, np.matrix):
+        A = np.matrix(A)
+
+    m, n = A.shape
+
+    Q = np.matrix(np.zeros([m, n]))
+    R = np.matrix(np.zeros([n, n]))
+
+    # loop column vectors in A
+    for j in range(n):
+        # inner product of A column vector j to previous vectors in Q
+        R[:j, j] = Q[:, :j].T * A[:, j]
+        
+        # subtract projections
+        Q[:, j] = A[:, j] - Q[:, :j] * R[:j, j]
+        
+        # compute norm and normalize
+        R[j, j] = np.linalg.norm(Q[:, j])
+        Q[:, j] = Q[:, j] / R[j, j]
+
+    return Q, R
+
+
+def modified_gram_schmidt(A):
+    """a modified variant of classical Gram-Schmidt method.
+    The idea is to orthogonalize against emerging set of vectors 
+    instead of against the orignal set. This method is expected
+    to be more numerically stable compared to the classical one.
+    """
+    if not isinstance(A, np.matrix):
+        A = np.matrix(A)
+
+    m, n = A.shape
+
+    Q = np.matrix(np.zeros([m, n]))
+    R = np.matrix(np.zeros([n, n]))
+
+    for j in range(n):
+
+        Q[:, j] = A[:, j]
+
+        # loop previous orthoganolized column vectors
+        for i in range(j):
+            R[i, j] = Q[:, i].T * A[:, j]
+            Q[:, j] = Q[:, j] - Q[:, i] * R[i, j]
+        
+        R[j, j] = np.linalg.norm(Q[:, j])
+        Q[:, j] = Q[:, j] / R[j, j]
+
+    return Q, R
+
+if __name__ == "__main__":
+    A = np.matrix([
+        [1, -1, 4],
+        [1, 4, -2],
+        [1, 4,  2],
+        [1, -1, 0]
+    ])
+    Q, R = modified_gram_schmidt(A)
+    print(Q.T * Q)
+    print(Q * R)
+    print()
---
+++ python/tests/test_qr.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pytest
+
+from numpy.testing import assert_equal
+from qr import classical_gram_schmidt, modified_gram_schmidt
+
+
+@pytest.fixture
+def A():
+    """a normal well conditioned matrix.
+    note that A is not a square matrix. The QR factorization of A = QR
+    doesn't imply Q * Q.T == I. Only Q.T * Q == I is true.
+    Q * Q.T == Q.T * Q == I is true only when A is a square matrix
+    """
+    return np.matrix([
+        [1, -1, 4],
+        [1, 4, -2],
+        [1, 4,  2],
+        [1, -1, 0]
+    ]
+)
+
+def test_classical_gram_schmidt(A):
+    Q, R = classical_gram_schmidt(A)
+    assert_equal(Q.T * Q, np.identity(3))
+    assert_equal(Q * R, np.matrix(A))
+
+
+def test_modified_gram_schmidt(A):
+    Q, R = modified_gram_schmidt(A)
+    assert_equal(Q.T * Q, np.identity(3))
+    assert_equal(Q * R, np.matrix(A))
+
+
